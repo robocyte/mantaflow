553384305d8f8ec43dbc4af07f3e3d2d7dba3b29
marylen <marie-lena.eckert@gmail.com>
2015-05-26 09:38:09 +0200

    corrected indentation

--- source/ex3.cpp
+++ source/ex3.cpp
@@ -40,30 +40,30 @@ KERNEL (single) void setupA(...){
 }
 
 PYTHON void diffuseTemperatureImplicit(...){
-		// solve A T = b
-		const int N = ...;
-		SparseMatrix<Real> A(N);
-		std::vector<Real> b(N);
+	// solve A T = b
+	const int N = ...;
+	SparseMatrix<Real> A(N);
+	std::vector<Real> b(N);
 
-		setupA(...);
-		setupB(...);
-		
-		// perform solve
-		Real pcg_target_residual = 1e-05;
-		Real pcg_max_iterations = 1000;
-		Real ret_pcg_residual = 1e10;
-		int  ret_pcg_iterations = -1;
+	setupA(...);
+	setupB(...);
+	
+	// perform solve
+	Real pcg_target_residual = 1e-05;
+	Real pcg_max_iterations = 1000;
+	Real ret_pcg_residual = 1e10;
+	int  ret_pcg_iterations = -1;
 
-		SparsePCGSolver<Real> solver;
-		solver.set_solver_parameters(pcg_target_residual, pcg_max_iterations, 0.97, 0.25);
+	SparsePCGSolver<Real> solver;
+	solver.set_solver_parameters(pcg_target_residual, pcg_max_iterations, 0.97, 0.25);
 
-		std::vector<Real> x(N);
-		for (int j = 0; j<N; ++j) { x[j] = 0.; }
+	std::vector<Real> x(N);
+	for (int j = 0; j<N; ++j) { x[j] = 0.; }
 
-		// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
-		solver.solve(A, b, x, ret_pcg_residual, ret_pcg_iterations, 0);
+	// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
+	solver.solve(A, b, x, ret_pcg_residual, ret_pcg_iterations, 0);
 
-		// x contains the new temperature values
-		fillT(...);
+	// x contains the new temperature values
+	fillT(...);
 }
 }; 
